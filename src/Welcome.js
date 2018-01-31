20e8302c37a87081198400e734546dcd558dd030
Lana Chiad <lanachiad7@gmail.com>
2018-01-31 15:10:36 -0500

    Add wrapper

--- src/Welcome.js
+++ src/Welcome.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
+import './Universal.css';
+import './Welcome.css';
 
 class Welcome extends Component {
   render() {
     return (
-      <div>
-        <h1>Welcome to the Harry Potter Quiz</h1>
-        <p>Are you ready to prove yourself as a true fan?</p>
-        <button onClick={this.props.beginQuiz}>I'm Ready</button>
+      <div id="welcome-screen">
+        <div className="game-wrapper">
+          <h1>Welcome to the Harry Potter Quiz</h1>
+          <p>Are you ready to prove yourself a true fan?</p>
+          <button onClick={this.props.beginQuiz}>I'm Ready</button>
+        </div>
       </div>
     );
   }
