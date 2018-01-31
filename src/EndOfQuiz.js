788e4ebdfc314fc2e39e63b39b133b1408387b30
Lana Chiad <lanachiad7@gmail.com>
2018-01-31 14:14:30 -0500

    Add a summary of how the user did after playing the quiz

--- src/EndOfQuiz.js
+++ src/EndOfQuiz.js
@@ -2,7 +2,17 @@ import React, { Component } from 'react';
 
 class EndOfQuiz extends Component {
   render() {
-    return <h1>It's done, bitches</h1>;
+    return (
+      <div>
+        <h2>Quills down, here's how you did</h2>
+        <p>
+          Final score: {this.props.finalScore}
+        </p>
+        <p>
+          That's a {this.props.finalScore / this.props.totalQuest * 100}% knowledge of all things Harry Potter.
+        </p>
+      </div>
+    );
   }
 }
 
