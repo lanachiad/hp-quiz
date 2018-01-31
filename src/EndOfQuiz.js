545f1aa7fa27d0731474beb10d81716026015894
Lana Chiad <lanachiad7@gmail.com>
2018-01-31 17:25:15 -0500

    Conditional results based on score and percent correct

--- src/EndOfQuiz.js
+++ src/EndOfQuiz.js
@@ -2,6 +2,20 @@ import React, { Component } from 'react';
 
 class EndOfQuiz extends Component {
   render() {
+    let result;
+
+    const percentCorrect = this.props.finalScore / this.props.totalQuest * 100;
+
+    if (percentCorrect >= 90) {
+      result = 'Impressive. You must be a Ravenclaw with wit like that.';
+    } else if (percentCorrect < 90 && percentCorrect >= 50) {
+      result = "You could have done better...but you also could've done worse.";
+    } else if (percentCorrect < 50 && percentCorrect >= 25) {
+      result = "I'm sure you have other traits that are worthwhile.";
+    } else {
+      result = 'Did you even try the quiz?';
+    }
+
     return (
       <div className="game-wrapper">
         <h2>Quills down, here's how you did</h2>
@@ -9,7 +23,10 @@ class EndOfQuiz extends Component {
           Final score: {this.props.finalScore}
         </p>
         <p>
-          That's a {this.props.finalScore / this.props.totalQuest * 100}% knowledge of all things Harry Potter.
+          That's a {percentCorrect}% knowledge of all things Harry Potter.
+        </p>
+        <p>
+          {result}
         </p>
       </div>
     );
