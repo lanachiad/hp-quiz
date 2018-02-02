974a8d9cf1e3a4f30e1979b5f28fb27d6b1ab896
Lana Chiad <lanachiad7@gmail.com>
2018-02-02 17:55:39 -0500

    Added an even 20 questions

--- src/data/all_questions.js
+++ src/data/all_questions.js
@@ -15,6 +15,11 @@ export default [
     correctAnswer: 'Misuse of Muggle Artefacts Office'
   },
   {
+    question: 'Where did the passage beneath the one-eyed witch statue lead to?',
+    answerChoices: ['Honeydukes', 'Shrieking Shack', 'The Three Broomsticks', 'The Room of Requirements'],
+    correctAnswer: 'Honeydukes'
+  },
+  {
     question: 'How many sickles are in a galleon?',
     answerChoices: ['20', '18', '17', '29', '15'],
     correctAnswer: '17'
@@ -35,6 +40,24 @@ export default [
     correctAnswer: 'Hungarian Horntail'
   },
   {
+    question: 'Where was the original headquarters for the Order of the Phoenix?'
+    answerChoices: ['The Burrow', '12 Grimmace Place', '24 Nurmengard Way' '12 Grimmauld Place'],
+    correctAnswer: '12 Grimmauld Place'
+  },
+  {
+    question: 'Which of these are NOT sweets sold at Honeydukes?',
+    answerChoices: [
+      'Toothflossing Stringmints',
+      'Cockroach Clusters',
+      'Pepper Imps',
+      'Acid Pops',
+      'Mice Pops',
+      'Broomstick Crunches',
+      'Fudge Flies'
+    ],
+    correctAnswer: 'Broomstick Crunches'
+  },
+  {
     question: "What is Voldemort's wand made out of?",
     answerChoices: ['Ash', 'Hawthorn', 'Yew', 'Vine wood'],
     correctAnswer: 'Yew'
@@ -43,6 +66,57 @@ export default [
     question: 'What did the Marauders call Snape to make fun of him?',
     answerChoices: ['Sev', 'Slytherus', 'Prince', 'Snivellus'],
     correctAnswer: 'Snivellus'
+  },
+  {
+    question: "What is the occupation of Hermione's parents?",
+    answerChoices: ['Doctors', 'Dentists', 'Professors', 'Lawyers'],
+    correctAnswer: 'Dentists'
+  },
+  {
+    question: 'What is the name of the French wizarding school?',
+    answerChoices: [
+      'Beauxbatons Academy of Magic',
+      'Beauxbarton Academy of Magic',
+      "L'école Français de la Magie",
+      'Beauxbatons Magic Academy'
+    ],
+    correctAnswer: 'Beauxbatons Academy of Magic'
+  },
+  {
+    question: 'What year was Hagrid in when he was expelled from Hogwarts?',
+    answerChoices: ['First', 'Fourth', 'Sixth', 'Third'],
+    correctAnswer: 'Third'
+  },
+  {
+    question: 'Who was headmaster at Hogwarts before Albus Dumbledore?',
+    answerChoices: ['Phineas Nigellus Black', 'Minerva McGonagall', 'Newton Scamander', 'Armando Dippet'],
+    correctAnswer: 'Armando Dippet'
+  },
+  {
+    question: 'How do you breed a basilisk?',
+    answerChoices: [
+      'They occur naturally',
+      'Hatch a toad egg underneath a chicken',
+      'Hatch a chicken egg underneath a toad',
+      'Make a sacrifice in the Chamber of Secrets',
+      'Ask Hagrid'
+    ],
+    correctAnswer: 'Hatch a chicken egg underneath a toad'
+  },
+  {
+    question: "What shape did Seamus Finnigan's boggart take?",
+    answerChoices: ['Banshee', 'Bloodstained, bandaged mummy', 'Disembodied living hand', 'Giant cobra', 'Dementor'],
+    correctAnswer: 'Banshee'
+  },
+  {
+    question: 'Who was the Quidditch referee?',
+    answerChoices: ['Minerva McGonagall', 'Madam Hooch', 'Oliver Wood', 'Poppy Pomfrey'],
+    correctAnswer: 'Madam Hooch'
+  },
+  {
+    question: 'Where is the Hufflepuff common room located near?',
+    answerChoices: ['Green houses', 'Astronomy tower', 'Great Hall', 'Kitchens'],
+    correctAnswer: 'Kitchens'
   },
   {
     question: 'All this time?',
